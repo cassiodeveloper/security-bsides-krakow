ca1db71251e6bad4ee97b88593795203237ede43
Cássio B. Pereira <cassio@cassiobp.com.br>
2025-02-24 20:31:48 +0100

    V1.0

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2024, 11, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2025, 9, 27, 8)}); // year, month, date, hour
         });
 });		
-
